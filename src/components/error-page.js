6aa8be782563c3f5e8576277a2c9c169526fb829
paul-altyfc <paul.m.dewey@gmail.com>
2019-07-20 16:33:30 +0100

    cosmetic changes

--- src/components/error-page.js
+++ src/components/error-page.js
@@ -4,9 +4,9 @@ import styles from './error-page.module.css';
 const ErrorPage = ({ text, err }) => {
   return (
     <div className={styles.main}>
-      <h1>{text || 'Something went wrong...'}</h1>
+      <h1 className={styles.textpos}>{text || 'Something went wrong...'}</h1>
       {err ? (
-        <p>
+        <p className={styles.textmess}>
           {err.message}. {err.response.data.msg}
         </p>
       ) : null}
